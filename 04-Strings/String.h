3e984378adda6017e868cb8667eac0f8c370d0e8
macedaezequiel <56090493+macedaezequiel@users.noreply.github.com>
2020-06-23 18:43:22 -0300

    Update String.h

--- 04-Strings/String.h
+++ 04-Strings/String.h
@@ -1,5 +1,7 @@
 #include<stdio.h>
 #include<stdlib.h>
+// Asumo que todas las cadenas que me van a pasar son finitas, el puntero no apunta a una cadena infinita
+
 
 int getLength(const char * text );
 int isEmpty( const char * text );
